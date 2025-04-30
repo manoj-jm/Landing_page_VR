dcaa8b7766152fb3b1cf182ec52f1ed7bb79adc5
manoj <jmanojjmvr49@gmail.com>
2025-04-30 13:04:25 +0530

    Add Hero component for showcasing virtual build tools

---
+++ src/components/Hero.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <>
+      <div className="lg:flex flex-col mt-13 items-center gap-4">
+        <h1 className="text-7xl text-center">
+          Virtual Build Tools{" "}
+          <span className="bg-gradient-to-l from-amber-800 to-amber-400  text-transparent bg-clip-text">
+            for <br />
+            developers
+          </span>
+        </h1>
+        <article className="text-center text-[#5a585aee]">
+          Empower your creativity and bring your VR-app ideas to liffe with our
+          intuitive development tools. <br />
+          Get Started today and turn your imagination into immersive reality
+        </article>
+        <div className="mt-8 space-x-20">
+          <a
+            href="#"
+            className="bg-gradient-to-r from-amber-600 to-amber-900 py-2 px-4 rounded "
+          >
+            Start for free
+          </a>
+          <a
+            href="#"
+            className=" py-2 px-4 rounded border border-white"
+          >
+            Documentation
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
