ce2d6d75239d3ec8c5090fd399cae2943a7b10ac
manoj <jmanojjmvr49@gmail.com>
2025-04-30 16:39:21 +0530

    testimonials for landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import Features from "./components/Features"
-import Hero from "./components/Hero"
-import Navbar from "./components/Navbar"
-import Pricing from "./components/Pricing"
-import WorkFlow from "./components/WorkFlow"
+import Features from "./components/Features";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Pricing from "./components/Pricing";
+import Testimonials from "./components/Testimonials";
+import WorkFlow from "./components/WorkFlow";
 
 function App() {
-  
   return (
     <>
-       <Navbar />
-       <Hero />
-       <Features />
-       <WorkFlow />
-       <Pricing />
+      <Navbar />
+      <Hero />
+      <Features />
+      <WorkFlow />
+      <Pricing />
+      <Testimonials />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
