109e5b7192e0e2057a381b907082e771d47d6b91
manoj <jmanojjmvr49@gmail.com>
2025-04-30 16:43:00 +0530

    Add Footer component and integrate it into the main App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Features from "./components/Features";
+import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Pricing from "./components/Pricing";
@@ -14,6 +15,7 @@ function App() {
       <WorkFlow />
       <Pricing />
       <Testimonials />
+      <Footer />
     </>
   );
 }
